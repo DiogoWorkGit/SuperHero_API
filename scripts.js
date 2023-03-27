af7ae0b7efb3e09e8513cebc25a03dbea873eb17
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-27 19:52:02 -0300

    Busca das histórias completas, apagando e procurando outras

--- scripts.js
+++ scripts.js
@@ -2,8 +2,15 @@
 function puxarcaps() {
     
     var nome = document.querySelector('.input_text').value;
-    nome_encoded = encodeURI(nome)
-    console.log(nome_encoded)
+    nome_encoded = encodeURI(nome);
+    console.log(nome_encoded);
+
+    const listaCmps = document.querySelectorAll('.lista_itens');
+
+    if (listaCmps.length > 0){
+        listaCmps.forEach(cmp => cmp.remove());
+    }
+
 
     fetch(`https://gateway.marvel.com/v1/public/characters?name=${nome_encoded}&limit=100&ts=1679874175&apikey=47b7d52506f06823ece0a03407fc41a1&hash=c7132295664b889624c030ac4aa40dcf`)
         .then(res => {
@@ -29,7 +36,8 @@ function puxarcaps() {
                         if (desc === ''){
                             desc = "Infelizmente a descrição desse capitulo não existe ou não está catalogada"
                         }
-                        const markup = `<li>Titulo do capítulo ${titulo} <br>Descrição do capítulo: ${desc}</li>`;
+
+                        const markup = `<li class="lista_itens">Titulo do capítulo: ${titulo} <br>Descrição do capítulo: ${desc}</li>`;
                     
                         document.querySelector('ul').insertAdjacentHTML('beforeend',markup);
                     })
