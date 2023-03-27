8e456789de7a524a1ff913e1dd6ad66ba2982c02
DiogoWorkGit <diogowork00@gmail.com>
2023-03-26 23:39:32 -0300

    aplicação já puxa os dados do personagem que o usuário quer encontrar

--- scripts.js
+++ scripts.js
@@ -1,40 +1,39 @@
 
-function 
+function puxarcaps() {
+    
+    var nome = document.querySelector('.input_text').value;
+    nome_encoded = encodeURI(nome)
+    console.log(nome_encoded)
 
+    fetch(`https://gateway.marvel.com/v1/public/characters?name=${nome_encoded}&limit=100&ts=1679874175&apikey=47b7d52506f06823ece0a03407fc41a1&hash=c7132295664b889624c030ac4aa40dcf`)
+        .then(res => {
+            return res.json();
+        })
+        .then(data =>{
+            var heroi = data.data.results
+            console.log(heroi)
+            heroi.forEach(element => {
+                var idhero = element.id
+                console.log(idhero)
 
-function puxarcaps(id)
-
-fetch('https://gateway.marvel.com/v1/public/characters?name=Iron%20Man&limit=100&ts=1679874175&apikey=47b7d52506f06823ece0a03407fc41a1&hash=c7132295664b889624c030ac4aa40dcf')
-    .then(res => {
-        return res.json();
-    })
-    .then(data =>{
-        var heroi = data.data.results
-        console.log(heroi)
-        heroi.forEach(element => {
-            var idhero = element.id
-            console.log(idhero)
-
-            fetch(`https://gateway.marvel.com/v1/public/stories?characters=${idhero}&limit=100&ts=1679874175&apikey=47b7d52506f06823ece0a03407fc41a1&hash=c7132295664b889624c030ac4aa40dcf`)
-            .then(res => {
-                return res.json();
-            })
-            .then(data =>{
-                var stories = data.data.results
-                console.log(stories)
-                stories.forEach(book => {
-                    titulo = book.title
-                    desc = book.description
-                    if (desc === ''){
-                        desc = "Infelizmente a descrição desse capitulo não existe ou não está catalogada"
-                    }
-                    const markup = `<li>Titulo do capítulo ${titulo} <br>Descrição do capítulo: ${desc}</li>`;
-        
-                    document.querySelector('ul').insertAdjacentHTML('beforeend',markup);
+                fetch(`https://gateway.marvel.com/v1/public/stories?characters=${idhero}&limit=100&ts=1679874175&apikey=47b7d52506f06823ece0a03407fc41a1&hash=c7132295664b889624c030ac4aa40dcf`)
+                .then(res => {
+                    return res.json();
                 })
-            })
-        });
-    })
-
-
-
+                .then(data =>{
+                    var stories = data.data.results
+                    console.log(stories)
+                    stories.forEach(book => {
+                        titulo = book.title
+                        desc = book.description
+                        if (desc === ''){
+                            desc = "Infelizmente a descrição desse capitulo não existe ou não está catalogada"
+                        }
+                        const markup = `<li>Titulo do capítulo ${titulo} <br>Descrição do capítulo: ${desc}</li>`;
+                    
+                        document.querySelector('ul').insertAdjacentHTML('beforeend',markup);
+                    })
+                })
+            });
+        })
+}
