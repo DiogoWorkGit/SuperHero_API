4bb30c1744faa115e885af32d26b0c081803b570
DiogoWorkGit <diogowork00@gmail.com>
2023-03-28 04:08:41 -0300

    Visualização dos personagens que estão sendo pesquisados feita

--- scripts.js
+++ scripts.js
@@ -11,17 +11,24 @@ function puxarcaps() {
         listaCmps.forEach(cmp => cmp.remove());
     }
 
+    const foto_hero = document.querySelectorAll('.imgchar');
+
+    if (foto_hero.length > 0){
+        foto_hero.forEach(cmp => cmp.remove());
+    }
+
 
     fetch(`https://gateway.marvel.com/v1/public/characters?name=${nome_encoded}&limit=100&ts=1679874175&apikey=47b7d52506f06823ece0a03407fc41a1&hash=c7132295664b889624c030ac4aa40dcf`)
         .then(res => {
             return res.json();
         })
         .then(data =>{
-            console.log(data)
             var heroi = data.data.results
             console.log(heroi)
             heroi.forEach(element => {
                 var idhero = element.id
+                const imgsrc = element.thumbnail.path + '.' + element.thumbnail.extension
+                console.log(imgsrc)
                 console.log(idhero)
 
                 fetch(`https://gateway.marvel.com/v1/public/stories?characters=${idhero}&limit=100&ts=1679874175&apikey=47b7d52506f06823ece0a03407fc41a1&hash=c7132295664b889624c030ac4aa40dcf`)
@@ -31,18 +38,21 @@ function puxarcaps() {
                 .then(data =>{
                     var stories = data.data.results
                     console.log(stories)
+
+                    const markup_img=`<div class="image_block"><img src="${imgsrc}" alt="" srcset="" class="imgchar"></div>`
+                    document.querySelector('.lista').insertAdjacentHTML('beforebegin',markup_img);
+
                     stories.forEach(book => {
                         titulo = book.title
                         desc = book.description
                         if (desc === ''){
                             desc = "Infelizmente a descrição desse capitulo não existe ou não está catalogada"
                         }
-
                         const markup = `<div class="lista_itens">
                         <p class = "item_titulo"><strong>Título</strong>: ${titulo}</p> 
                         <p class = "item_desc"><strong>Descrição</strong>: ${desc}</p>
                         </div>`;
-                    
+                        
                         document.querySelector('.lista').insertAdjacentHTML('beforeend',markup);
                     })
                 })
